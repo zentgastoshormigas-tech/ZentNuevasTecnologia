fdc0daaef0d3cedc8714f3aad29e3a03f45bffda
Julio Carrillo <carrillojulio470@gmail.com>
2026-04-27 19:40:21 -0500

    feat creamos archivo para limpieza de datos de categoria y se organiza para las demas entidades

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import pandas as pd
+
+#importa simulaciones
 from utils.simulacionCategoria import simular_tabla_categoria
+
+#zona limpieza
+from notebook.limpieza import limpiar_datos_categoria
+
+#importa generador de json y csv
 from notebook.generador import crear_json, crear_csv
 
+
+#crear simulacion
 simulacionDeCategoria = simular_tabla_categoria(10)
+#ordenando simulaciones
 simulacionOrdenadaDeCategoria = pd.DataFrame(simulacionDeCategoria)
 
-crear_json(simulacionOrdenadaDeCategoria, "data/datosCategoria.json")
-crear_csv(simulacionOrdenadaDeCategoria, "data/datosCategoria.csv")
+#limpiar set de datos
+simulaciones_limpias_categoria = limpiar_datos_categoria(simulacionOrdenadaDeCategoria)
+
+
+crear_json(simulaciones_limpias_categoria, "data/datosCategoria.json")
+crear_csv(simulaciones_limpias_categoria, "data/datosCategoria.csv")
